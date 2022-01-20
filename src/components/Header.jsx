5ed5ab069140b96a3661101a3072b19c54bb567b
DannyPoizot <poizot.dan@gmail.com>
2022-01-20 21:03:11 +0100

    initilal files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+         <nav>
+            <img src="logo.svg" width="60px" alt="img" />
+            <span className="factTitle">ReactFacts</span>
+            <span className="navItem">React Course - Project 1</span>
+         </nav>
+        </header>
+    );
+}
+
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+const page = (
+  <div>
+    <Header />,
+    <MainContent />,
+    <Footer />
+  </div>
+);
+
+ReactDOM.render(
+  page,
+  document.getElementById('root')
+);
